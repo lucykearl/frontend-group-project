fb58ea88a370c90a904f626f1cc060b6ea7e6337
lucykearl <104901670+lucykearl@users.noreply.github.com>
2022-10-27 13:38:39 +0100

    Routes created

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,19 @@
 import "../styles/app.css";
+import NavBar from "./NavBar";
+import "../styles/navbar.css";
+import { Routes, Route } from "react-router-dom";
+import AddSessionPage from "./AddSession";
+import HistoryPage from "./History";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/addsession" element={AddSessionPage} />
+        <Route path="/history" element={HistoryPage} />
+      </Routes>
       <h2>REP</h2>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "../styles/navbar.css";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="nav-bar">
+      <ul>
+        <li className="navbar-links-item">
+          <Link className="navbar-item" to="/">
+            Home
+          </Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link className="navbar-item" to="/AddSessionPage">
+            +
+          </Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link className="navbar-item" to="/HistoryPage">
+            History
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
