5e2a0148eff9f96b470ccfcb1020f973a78e7966
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-10-29 15:12:53 +0100

    changed exercise table so each exercise has id, fixed get request

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import {Autocomplete, TextField} from '@mui/material';
 import axios from "axios";
 
-const AutoComplete = () => {
+const AutoComplete = (onSelect) => {
 
   const [exercises, setExercises] = useState([])
+  const [selected, setSelected] = useState('')
+  
+  console.log(selected)
+  onSelect=(selected)
 
   useEffect(() => {
-    axios.get('http://localhost:5000/exercises').then(({ data }) => setExercises(data[0].exercises))
+    axios.get('http://localhost:5000/exercises').then(({ data }) => setExercises(data))
   }, [])
 
   return (
@@ -16,6 +20,7 @@ const AutoComplete = () => {
       options={exercises}
       groupBy={(option) => option.type}
       getOptionLabel={(option) => option.title}
+      onChange={(event, value) => setSelected(value.title)}
       sx={{ width: 300 }}
       renderInput={(params) => <TextField {...params} label="Select Exercise" />}
     />
