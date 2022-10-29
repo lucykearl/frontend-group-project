0854305285f7a2004780292724b2b27352f557fc
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-10-29 16:07:23 +0100

    handleSelect works, need to add validation to make sure the state is truthy before it does anything.

--- src/components/AddSession.js
+++ src/components/AddSession.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+import AddSessionCard from "./AddSessionCard";
 import AutoComplete from "./AutoComplete";
 
 const AddSessionPage = () => {
 
+  const [selectedOption, setSelectedOption] = useState([]);
+
+  const handleSelect = (selected) => {
+    setSelectedOption(selected)
+  }
+
   return (
-    <AutoComplete />
+    <div className="add-session-page">
+      <AutoComplete onSelect={handleSelect}/>
+      <AddSessionCard selected={selectedOption}/>
+    </div>
+    
   )
 };
 
--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -2,28 +2,28 @@ import React, { useEffect, useState } from "react";
 import {Autocomplete, TextField} from '@mui/material';
 import axios from "axios";
 
-const AutoComplete = (onSelect) => {
+const AutoComplete = ({onSelect}) => {
 
   const [exercises, setExercises] = useState([])
   const [selected, setSelected] = useState('')
-  
-  console.log(selected)
-  onSelect=(selected)
 
   useEffect(() => {
     axios.get('http://localhost:5000/exercises').then(({ data }) => setExercises(data))
   }, [])
 
   return (
-    <Autocomplete
-      id="grouped-exercises"
-      options={exercises}
-      groupBy={(option) => option.type}
-      getOptionLabel={(option) => option.title}
-      onChange={(event, value) => setSelected(value.title)}
-      sx={{ width: 300 }}
-      renderInput={(params) => <TextField {...params} label="Select Exercise" />}
-    />
+    <div className="auto-complete">
+      <Autocomplete
+        id="grouped-exercises"
+        options={exercises}
+        groupBy={(option) => option.type}
+        getOptionLabel={(option) => option.title}
+        onChange={(event, value) => setSelected(value.title)}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Select Exercise" />}
+      />
+      <button type="button" onClick={() => onSelect(selected)}>Add</button>
+    </div>
   )
 };
 
