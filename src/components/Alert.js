925473c42be92d9dec1c645212b8f772bf253b89
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-11-07 23:46:12 +0000

    added alerts to prevent adding null values and duplicate exercises

---
+++ src/components/Alert.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Alert = ({ message, success }) => {
+  if (!message) return null;
+
+  return (
+    <div className={`alert alert-${success ? "success" : "error"}`}>
+      {message}
+    </div>
+  );
+};
+
+export default Alert;
+
+Alert.defaultProps = {
+  success: false,
+};
+
+Alert.propTypes = {
+  message: PropTypes.string.isRequired,
+  success: PropTypes.bool,
+};
